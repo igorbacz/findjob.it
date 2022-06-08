49ebb81b7f90d108d751d123b7669680201656d7
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-06-08 13:57:06 +0200

    kk

--- web/justjoin-it/src/App.tsx
+++ web/justjoin-it/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { TopBar } from "./components/topBar/TopBar";
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <h1>Iwooona</h1>
     </div>
   );
