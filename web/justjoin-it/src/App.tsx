a74f1f7590e11084d14c712815191829f03f719c
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-06-09 14:24:27 +0200

    basic structure of top bar

--- web/justjoin-it/src/App.tsx
+++ web/justjoin-it/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <TopBar />
-      <h1>Iwooona</h1>
     </div>
   );
 }
--- web/justjoin-it/src/components/topBar/TopBar.tsx
+++ web/justjoin-it/src/components/topBar/TopBar.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import "./style.css";
 
 export const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbar__left">
+        <h1 className="topbar__logo">
+          <a>justjoin.it</a>
+        </h1>
+        <p className="topbar__logo">#1 Job Board for tech industry in Europe</p>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider"></span>
+        </label>
+      </div>
+      <div className="topbar__items">
+        <h5 className="topbar__item">
+          <a>Offers</a>
+        </h5>
+        <h5 className="topbar__item">
+          <a>Brand Stories</a>
+        </h5>
+        <h5 className="topbar__item">
+          <a>Geek</a>
+        </h5>
+        <h5 className="topbar__item">
+          <a>Matchmaking</a>
+        </h5>
+      </div>
+      <div className="topbar__buttons">
+        <button className="button--light" type="button">
+          Post a Job
+        </button>
+        <button className="button--pink" type="button">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
 };
