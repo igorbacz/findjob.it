83de8b7e892a57be7c5823542bca9a76f03fbc50
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-08-31 16:23:49 +0200

    finished markers

--- web/justjoin-it/src/components/OpenStreetMap/OpenStreetMap.tsx
+++ web/justjoin-it/src/components/OpenStreetMap/OpenStreetMap.tsx
@@ -3,36 +3,19 @@ import L from "leaflet";
 import { data, stackIcons } from "../../data";
 import { useNavigate } from "react-router-dom";
 
-// const iconMarkup = renderToStaticMarkup(<FaHtml5 />);
-// const markerIcon = L.divIcon({
-//   html: <FaHtml5 />,
-// });
-
-// const iconMarkup = data.map((icon)=>)
-
 export const OpenStreetMap = () => {
   let navigate = useNavigate();
 
-  // const StackIcon = L.icon({
-  //   // iconUrl: "https://justjoin.it/static/media/testing.4e552286.svg",
-  //   iconUrl:
-
-  // });
-
-  const iconUrlFind = data.map((icon) => {
-    const highestLevelStack = icon.techStack.find((stack) => stack.value === 5);
+  const iconUrlFind = (city: any) => {
+    const highestLevelStack = city.techStack.find((stack: any) => stack.value === 5);
     const iconOject = stackIcons.find((stack) => stack.stack === highestLevelStack.stackName);
     const iconUrl = iconOject.url;
-    console.log(iconUrl);
-    return iconUrl;
-  });
-
-  const iconStackUrl = iconUrlFind[1];
-
-  const LeafIcon = new L.Icon({
-    iconSize: [25, 25],
-    iconUrl: iconStackUrl,
-  });
+    const LeafIcon = new L.Icon({
+      iconSize: [25, 25],
+      iconUrl: iconUrl,
+    });
+    return LeafIcon;
+  };
 
   return (
     <MapContainer center={[52.291335, 19.088525]} zoom={6} scrollWheelZoom={false}>
@@ -42,7 +25,6 @@ export const OpenStreetMap = () => {
       />
       {data.map((city) => (
         <Marker
-          // touchZoom={true}
           position={[city.geolocation.latitude, city.geolocation.longitude]}
           key={city._id}
           eventHandlers={{
@@ -50,7 +32,7 @@ export const OpenStreetMap = () => {
               return navigate(`/offer/${city._id}`);
             },
           }}
-          icon={LeafIcon}
+          icon={iconUrlFind(city)}
         ></Marker>
       ))}
     </MapContainer>
