9f0c39096c0a4424cf99112d60a373ded2256f26
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-11-08 12:52:55 +0100

    from master

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./service/store";
-import { getOffersData } from "./service/offers/actions";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-// store.dispatch(getOffersData());
 root.render(
   <BrowserRouter>
     <Provider store={store}>
@@ -16,5 +13,3 @@ root.render(
     </Provider>
   </BrowserRouter>
 );
-
-reportWebVitals();
