8f304eda0ad2b12afb6313dcdebd935c1d9a8bbe
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-11-16 10:55:16 +0100

    Updates

--- frontend/src/pages/bigOfferPage/BigOfferPage.tsx
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from "@mui/material";
-import { theme } from "../../theme";
-import { LargeView } from "./components/LargeView";
-import { DefaultView } from "./components/DeafultView";
-
-export const BigOfferPage = () => {
-  const isMatchLarge = useMediaQuery(theme.breakpoints.down("lg"));
-  return <>{isMatchLarge ? <LargeView /> : <DefaultView />}</>;
-};
--- frontend/src/service/user/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "../../types/types";
-
-export const userDataSelector = (state: RootState) => state.user;
-
-export const isAuthenticatedSelector = (state: RootState) => state.user.isAuthenticated;
