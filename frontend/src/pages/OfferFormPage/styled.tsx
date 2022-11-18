6e39a3a2f802b077f69880333605bd1b11836e26
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-11-18 14:29:31 +0100

    changes in style of offerForm

--- frontend/src/pages/OfferFormPage/styled.tsx
+++ frontend/src/pages/OfferFormPage/styled.tsx
@@ -3,8 +3,6 @@ import { Box } from "@mui/material";
 
 export const FormPageContainer = styled(Box)`
   padding: 30px;
-  padding-top: 35%;
-  height: 100%;
   background-color: #f3f6f8;
   display: flex;
   justify-content: center;
