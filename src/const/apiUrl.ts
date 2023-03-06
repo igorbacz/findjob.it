44c8f912910e1ac590982ea3f7b31b5896ccf506
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2023-03-06 10:06:12 +0100

    changed api url

--- src/const/apiUrl.ts
+++ src/const/apiUrl.ts
@@ -1,5 +1,5 @@
-// const apiUrl = "https://894xsxeql5.execute-api.us-east-1.amazonaws.com";
+const apiUrl = "https://894xsxeql5.execute-api.us-east-1.amazonaws.com";
 
-const apiUrl = "http://localhost:3000";
+// const apiUrl = "http://localhost:3000";
 
 export default apiUrl;
