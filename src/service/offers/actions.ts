d37cf576ee4724519a501c13b8c46d26e4d0cb03
Iwona Gorbacz <i.gorbacz@gmail.com>
2023-03-14 15:32:23 +0100

    changed getOffers func

--- src/service/offers/actions.ts
+++ src/service/offers/actions.ts
@@ -8,7 +8,7 @@ export const getOffers = async () => {
     throw new Error("Could not fetch offers data");
   }
   const data = await response.json();
-  return JSON.parse(data);
+  return data;
 };
 
 export const getOffersData = () => {
