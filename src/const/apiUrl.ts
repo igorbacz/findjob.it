741ee5cfa2318abadfcf6f0d2df36aa06ab3b59d
Iwona Gorbacz <i.gorbacz@gmail.com>
2023-03-14 11:01:10 +0100

    changed api url

--- src/const/apiUrl.ts
+++ src/const/apiUrl.ts
@@ -1,4 +1,6 @@
-const apiUrl = "https://894xsxeql5.execute-api.us-east-1.amazonaws.com";
+// const apiUrl = "https://894xsxeql5.execute-api.us-east-1.amazonaws.com";
+
+const apiUrl = "https://d30edyl7j9.execute-api.us-east-1.amazonaws.com";
 
 // const apiUrl = "http://localhost:3000";
 
