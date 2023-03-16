833ed53cffe1900389d4b8866b18088acbeed510
Iwona Gorbacz <i.gorbacz@gmail.com>
2023-03-16 14:29:17 +0100

    new api url

--- src/const/apiUrl.ts
+++ src/const/apiUrl.ts
@@ -1,6 +1,6 @@
-// const apiUrl = "https://d30edyl7j9.execute-api.us-east-1.amazonaws.com";
+const apiUrl = "https://894xsxeql5.execute-api.us-east-1.amazonaws.com";
 
-const apiUrl = "https://nestapi.herokuapp.com";
+// const apiUrl = "https://nestapi.herokuapp.com";
 
 // const apiUrl = "http://localhost:3000";
 
